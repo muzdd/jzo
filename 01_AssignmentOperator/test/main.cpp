4e5f7a510b4801072aaa379d8be51b48153d353f
Travis-Lee <lixiang.fr@hotmail.com>
2022-05-15 13:19:27 +0800

    optimize project

--- 01_AssignmentOperator/test/main.cpp
+++ 01_AssignmentOperator/test/main.cpp
@@ -1,9 +1,8 @@
 #include "AssignmentOperator.h"
 #include <stdio.h>
 
-int main(int argc, char* argv[])
+void Test1()
 {
-    
     printf("Test1 begins:\n");
 
     char* text = "Hello world";
@@ -17,9 +16,53 @@ int main(int argc, char* argv[])
     printf("The actual result is: ");
     str2.Print();
     printf(".\n");
+}
 
+void Test2()
+{
+    printf("Test2 begins:\n");
 
+    char* text = "Hello world";
 
+    CMyString str1(text);
+    str1 = str1;
+
+    printf("The expected result is: %s.\n", text);
+
+    printf("The actual result is: ");
+    str1.Print();
+    printf(".\n");
+}
+
+void Test3()
+{
+    printf("Test3 begins:\n");
+
+    char* text = "Hello world";
+
+    CMyString str1(text);
+    CMyString str2, str3;
+    str3 = str2 = str1;
+
+    printf("The expected result is: %s.\n", text);
+
+    printf("The actual result is: ");
+    str2.Print();
+    printf(".\n");
+
+    printf("The expected result is: %s.\n", text);
+
+    printf("The actual result is: ");
+    str3.Print();
+    printf(".\n");
+}
+
+int main(int argc, char* argv[])
+{
+    Test1();
+    Test2();
+    Test3();
+    
     return 0;
 }
 
